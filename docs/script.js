51e63a0859a4424bed7e79a04a75d474f4cc0305
ethan mckissic <mckissicethan@gmail.com>
2024-07-22 21:59:34 -0400

    Added Navbar w/ responsiveness
    will fix styling later

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+function showSidebar(){
+    const sidebar = document.getElementsByClassName("sidebar")[0].style.display = "flex";
+}
+
+function hideSidebar(){
+    const sidebar = document.getElementsByClassName("sidebar")[0].style.display = "none";
+}
